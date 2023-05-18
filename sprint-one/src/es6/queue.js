20d727642ea81cbce228bb63276edb6f61b53aec
Brett St. George <brett.stgeorge@gmail.com>
2023-05-18 12:57:24 -0400

    Data Structures sprint 1 assignment complete.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.lastIndex = 0;
   }
 
+  enqueue(value) {
+    for (var i = this.lastIndex; i > 0; i--) {
+      this.storage[i] = this.storage[i - 1];
+    }
+    this.storage[0] = value;
+    this.lastIndex++;
+  }
+
+  dequeue() {
+    this.lastIndex--;
+    let removedValue = this.storage[this.lastIndex];
+    delete this.storage[this.lastIndex];
+    return removedValue;
+  }
+
+  size() {
+    return Math.max(this.lastIndex, 0);
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.lastIndex = 0;
   }
 
+  push(value) {
+    this.storage[this.lastIndex] = value;
+    this.lastIndex++;
+  }
+
+  pop() {
+    this.lastIndex--;
+    let poppedValue = this.storage[this.lastIndex];
+    delete this.storage[this.lastIndex];
+    return poppedValue;
+  }
+
+  size() {
+    return Math.max(this.lastIndex, 0);
+  }
 }
